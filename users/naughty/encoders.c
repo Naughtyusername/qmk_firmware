ed27ed22b926446866c2f9228a83fd59f327e61c
NaughtyDesktop <Whatsahardscope@gmail.com>
2020-02-23 03:58:21 -0800

    userspace

--- users/naughty/encoders.c
+++ users/naughty/encoders.c
@@ -1,3 +1,43 @@
+#include "encoders.h"
 
+void encoder_update_user(uint8_t index, bool clockwise) {
+  // Left encoder
+  if (index == 0) {
+    switch(biton32(layer_state)) {
+    case _BASE:
+      if (clockwise) {
+        tap_code(KC_VOLU);
+      } else {
+        tap_code(KC_VOLD);
+      }
+      break;
+    case _LOWER:
+    case _RAISE:
+    case _ADJUST:
+    case _VIKEYS:
+    case _GAMING:
+    case _ROGUE:
+    default: break;
+    }
+  }
+  // Right encoder
+  if (index == 1) {
+    switch(biton32(layer_state)) {
+    case _BASE:
+      if (clockwise) {
+        tap_code(KC_PGDN);
+      } else {
+        tap_code(KC_PGUP);
+      }
+      break;
+    case _LOWER:
+    case _RAISE:
+    case _ADJUST:
+    case _VIKEYS:
+    case _GAMING:
+    case _ROGUE:
+    default: break;
+    }
 
-// encoders
+  }
+}
